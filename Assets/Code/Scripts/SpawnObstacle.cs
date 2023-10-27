b2c6101014226cb762df0d4861b099ff657cdaf9
xUnderGame <unaigiz1@gmail.com>
2023-10-27 15:51:58 +0200

    Fixed and optimized most of the code, added a speed variable to moveObstacle and added the script to the soul object aswell

--- Assets/Code/Scripts/SpawnObstacle.cs
+++ Assets/Code/Scripts/SpawnObstacle.cs
@@ -1,47 +1,60 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnObstacle : MonoBehaviour
 {
+    public GameObject soulPrefab;
+    public GameObject duckPrefab;
+    public GameObject jumpPrefab;
+    public GameObject lanePrefab;
+    [DoNotSerialize] public float frameCounter;
+    [DoNotSerialize] public float defaultCounter;
 
-    public GameObject duck;
-    public GameObject jump;
-    public GameObject lane;
-    private int frameCounter;
-    // Start is called before the first frame update
     void Start()
     {
-        frameCounter = 300;
+        defaultCounter = 3.0f;
+        frameCounter = defaultCounter;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        frameCounter--;
         if (frameCounter <= 0)
         {
-            int tim = Random.Range(0,271);
-            makeObstacle();
-            frameCounter = 210 + tim;
+            MakeObstacle();
+            frameCounter = defaultCounter + Random.Range(0, 1);
         }
+        frameCounter -= Time.deltaTime;
     }
-    private void makeObstacle()
-    {
-        int obs = Random.Range(0, 301);
-        if (obs == 0)
-            Instantiate(lane, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
-        else if (obs <= 100)
-            Instantiate(jump, transform.position + new Vector3(0, -1.5f, 0), Quaternion.identity);
-        else if (obs <= 200)
-            Instantiate(duck, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
-        else if (obs <= 300)
-        {
-            GameObject sol = Instantiate(soul, transform.position + new Vector3(0, 2.3f, 0), Quaternion.identity, soul.transform.parent);
-            sol.transform.localScale = new Vector3(3f, 3f, 3f);
-            //sol.soulsDisplay = score;
-        }
 
+    // Creates an obstacle on a randomly chosen lane
+    private void MakeObstacle()
+    {
+        int obs = Random.Range(0, 4);
+        switch(obs) {
+            // Wall
+            case 0:
+                Instantiate(lanePrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+                break;
+
+            // Object to jump
+            case 1:
+                Instantiate(jumpPrefab, transform.position + new Vector3(0, -1.5f, 0), Quaternion.identity);
+                break;
+
+            // Object to duck
+            case 2:
+                Instantiate(duckPrefab, transform.position + new Vector3(0, 2, 0), Quaternion.identity);
+                break;
+
+            // Spawns a soul
+            case 3:
+                GameObject soulObject = Instantiate(soulPrefab, transform.position + new Vector3(0, 2.3f, 0), Quaternion.identity, soulPrefab.transform.parent);
+                soulObject.transform.localScale = new Vector3(3f, 3f, 3f);
+                //soulObject.soulsDisplay = score;
+                break;
+        }
     }
 }
