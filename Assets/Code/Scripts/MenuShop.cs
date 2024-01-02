c4dd677c8efa23767d66654d3f3aaab0f9f40e0c
adrian.rodriguez <adrian.rodriguez.7e6@itb.cat>
2024-01-02 18:39:45 +0100

    added the own souls to the menu shop

--- Assets/Code/Scripts/MenuShop.cs
+++ Assets/Code/Scripts/MenuShop.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuShop : MonoBehaviour
 {
+    public UserData userData;
+
+    public Text totalSouls;
+
     public void Start()
     {
         GetTotalSouls();
     }
 
-    public void GetTotalSouls()
-    {
-        
-    }
+    public void GetTotalSouls() => totalSouls.text = Convert.ToString(userData.souls);
 
     public void Return()
     {
