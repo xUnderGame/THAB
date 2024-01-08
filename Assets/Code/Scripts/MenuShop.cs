e44dca9d20a6aa205f49885f8d443e1097c2c796
adrian.rodriguez <adrian.rodriguez.7e6@itb.cat>
2024-01-08 15:57:59 +0100

    reorganizar codigo

--- Assets/Code/Scripts/MenuShop.cs
+++ Assets/Code/Scripts/MenuShop.cs
@@ -5,31 +5,18 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UserData;
 
 public class MenuShop : MonoBehaviour
 {
-    public UserData userData;
-
-    public Image image;
+    /*public Image image;
     
     public TextMeshPro totalSouls;
     public TextMeshPro improveForcefieldSouls;
     //public TextMeshPro improveMagnetSouls;
 
-    public UserData.Forcefield forcefield = new UserData.Forcefield();
-
-    public UserData.L1 l1 = new UserData.L1();
-    public UserData.L2 l2 = new UserData.L2();
-    public UserData.L3 l3 = new UserData.L3();
-    public UserData.L4 l4 = new UserData.L4();
-    public UserData.L5 l5 = new UserData.L5();
-
-
     public void Start()
     {
         GetTotalSouls();
-        forcefield.currentLevel = 1;
         SetPrice();
     }
 
@@ -44,7 +31,7 @@ public class MenuShop : MonoBehaviour
     //Set the price of next level
     public void SetPrice()
     {
-        switch (SetNextLevel(forcefield.currentLevel))
+        switch (SetNextLevel(JsonManager.Instance.userData.upgrades.forcefield.currentLevel))
         {
             case "L1":
                 improveForcefieldSouls.text = Convert.ToString(l1.nextUpgradeCost);
@@ -74,5 +61,5 @@ public class MenuShop : MonoBehaviour
     }
 
     // Return to menu
-    public void Return() => SceneManager.LoadScene(0);
+    public void Return() => SceneManager.LoadScene(0);*/
 }
