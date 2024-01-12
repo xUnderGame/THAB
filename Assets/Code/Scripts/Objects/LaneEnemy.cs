b4ea4131bc8fad2193942f55df3ffaef5011ae9a
xUnderGame <unaigiz1@gmail.com>
2024-01-12 14:53:59 +0100

    Fixed lane enemy despawning after swapping and reworked enemy despawning

--- Assets/Code/Scripts/Objects/LaneEnemy.cs
+++ Assets/Code/Scripts/Objects/LaneEnemy.cs
@@ -10,13 +10,17 @@ public class LaneEnemy : LaneBehaviour, IDamageable
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.position.x <= 4 && !hasSwapped)
+        // Swaps lanes when position passes a threshold
+        if (transform.position.x <= 4 && !hasSwapped)
         {
             hasSwapped = true;
             SwapLane(gameObject.layer == 7,
                 GetComponent<Rigidbody2D>(),
                 gameObject);
         }
+
+        // Destroys enemy after a position threshold
+        if (transform.position.x <= -20) Destroy(gameObject);
     }
 
     // Collision actions
